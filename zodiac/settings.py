7434823104d3fdd779ed29069052249ffe21d6f9
wenxiao Lv <2064747320@qq.com>
2025-03-02 20:26:46 +0800

    add the audio

--- zodiac/settings.py
+++ zodiac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-umxsmfz+=*hus6h*aufmmx78!ptda9a^a+wlxm!0c!^vk99c1*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['47.121.137.60', 'app7394.acapp.acwing.com.cn']
+ALLOWED_HOSTS = ['47.121.137.60', 'app7394.acapp.acwing.com.cn', '127.0.0.1']
 
 
 # Application definition
